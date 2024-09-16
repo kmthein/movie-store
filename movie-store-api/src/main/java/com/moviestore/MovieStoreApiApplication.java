823344365edd64fb4a7edd55cab4b1dcbaa20daa
kmthein <kmthein96.mm@gmail.com>
2024-09-17 02:22:33 +0630

    project config

---
+++ movie-store-api/src/main/java/com/moviestore/MovieStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.moviestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieStoreApiApplication.class, args);
+    }
+
+}
---
+++ movie-store-api/src/test/java/com/moviestore/MovieStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
